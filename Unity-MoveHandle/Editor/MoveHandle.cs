22246b20730b80275cd5b687951db99cb99e13ba
sashcode <sashcode@gmail.com>
2013-01-31 01:35:55 +0900

    undo 1

--- Unity-MoveHandle/Editor/MoveHandle.cs
+++ Unity-MoveHandle/Editor/MoveHandle.cs
@@ -9,9 +9,14 @@ public class MoveHandle  : Editor
 	void OnSceneGUI ()
 	{
 		LookAtPoint t = target as LookAtPoint;
+		Undo.SetSnapshotTarget (t, "Moved Object Around");
 		t.lookAtPoint = Handles.PositionHandle (t.lookAtPoint, Quaternion.identity);
 		if (GUI.changed) {
 			EditorUtility.SetDirty (t);
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			Undo.CreateSnapshot ();
+			Undo.RegisterSnapshot ();
 		}
 	}
 }
